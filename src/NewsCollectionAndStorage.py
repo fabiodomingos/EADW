454b0fb0d8167b6e39ba2bd0ada1566734df13ff
guntty <guntty@194.210.220.117>
2013-04-05 19:36:42 +0100

    update info

--- src/NewsCollectionAndStorage.py
+++ src/NewsCollectionAndStorage.py
@@ -4,10 +4,20 @@ Created on 2013/04/05
 @author: goncalocarito
 @authro: fabiodomings
 
-This module has the functions to pull public news feed, identify and collect
-new news items, and store them in a common repository for further processing
-The news collection should be performed on demand and new news items found should
-be added to the repository.
-We have to decide if it makes sense to have the collections and storage.
+1) pull public news feed
+2) identify 
+3) collect new news items
+4) store them in a common repository for further processing
+
+- Notes:
+The news collection should be performed on demand - invoking a function?
+and new news items found should be added to the repository.
+
+- Doubts to decide:
+Where do we'll store the new items? Which plataform/type/file?
 
 '''
+
+#TODO: identify news items
+#TODO: identify new news items
+#TODO: collect and store -> common repository
