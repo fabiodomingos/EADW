9014f60fc2bc92c8f7111a1c0b4b9f659a2e5ad2
guntty <guntty@194.210.220.117>
2013-04-05 15:14:35 +0100

    funcao teste

---
+++ src/FirstModule.py
@@ -0,0 +1,17 @@
+'''
+Created on 2013/04/05
+
+@author: goncalocarito
+'''
+
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y = 5
+    return y +a
+  
+print add(1,2)
+print addFixedValue(1) 
+
+
