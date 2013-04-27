fed29ffe2d894c31e91810c08cd8212f6c07b64d
guntty <guntty@172.20.81.155>
2013-04-27 15:58:56 +0100

    code reorganization

--- src/NewsCollectionAndStorage.py
+++ src/NewsCollectionAndStorage.py
@@ -9,102 +9,171 @@ Created on 2013/04/05
 4) store them in a common repository for further processing
 
 - Notes:
-The news collection should be performed on demand - invoking a function?
+The news collection should be performed on demand
 and new news items found should be added to the repository.
 
-- Doubts to decide:
-Where do we'll store the new items? Which platform/type/file?
-Use sqlit3?
 
 '''
-### IMPORTS
-import feedparser
-import sqlite3
+# # OPTIONAL TODO
+# # Implement the real articles linked by the feed
+# # to do that is necessary to explore STORED/JUST INDEXED ITEMS and how it works
+
+# # MORE THAN OPTIONAL TODO
+# # Explore whoosh documentation: http://pythonhosted.org/Whoosh/index.html
+# # and do awesome things with that
+
+
+# ## IMPORTS
 from datetime import datetime
-from whoosh.index import create_in
-from whoosh.index import open_dir   
-from whoosh.fields import Schema
-from whoosh.fields import DATETIME
-from whoosh.fields import TEXT
-from whoosh.qparser import QueryParser, OrGroup
+from whoosh.fields import DATETIME, Schema, TEXT
+from whoosh.index import create_in, open_dir
+import NewsSearch
+import feedparser
 import os.path
 import re
+import sqlite3
 
 
-### newsFeedName
-newsFeedNames =['dn','jn']
+# ## INITS
+
+# newsFeedName
+newsFeedNames = ['dn', 'jn']
 newsFeedNamesEnum = enumerate(newsFeedNames)
-
-### FEEDs URLS
+# FEEDs URLS
 newsFeedDN = "http://feeds.dn.pt/DN-Politica"
 newsFeedJN = "http://feeds.jn.pt/JN-Politica"
-
-conn = sqlite3.connect("teste.db")
-cursor = conn.cursor()
+newsFeedTeste = r'/Users/guntty/git/EADW/src/teste.xml'
 
 
-# create a new Table with titulo, conteudo, data
-# this function is just called once
-def createTable():
-    # create a table
-    #cursor.execute("""CREATE TABLE newsfeeds
-    #                 (newsfeedname text, title text, content text, date text) 
-    #              """)
-    cursor.execute('''CREATE TABLE news (titulo TEXT, conteudo TEXT, date TEXT)''')
-    
-# create a new Index for Whoosh
-# @returns: ix - Index Object 
-def createIndexWhoosh():
-    ## conteudo pode ser 
-    schema = Schema(content=TEXT(stored=True), date=DATETIME, title=TEXT(stored=True))  
+# ## MAIN FUNCTIONS    
+
+def createIndex():
+    """ create a new Whoosh Index with the schema: title, content and date of the news feed
+    @returns: the Index object """
+    schema = Schema(content=TEXT(stored=True), date=DATETIME(stored=True), title=TEXT(stored=True))
+    # verify if the index dir exists in filesystem 
+    # if not create a new one after creating the index itself
     if not os.path.exists("index"):
         os.mkdir("index")
     ix = create_in("index", schema)
     return ix
 
-# AUX Function
-# Insert new 'New' on the table news
-def addNew(titulo, conteudo,date):
-    cursor.execute('''INSERT INTO news (titulo, conteudo, date)
-    VALUES (?,?,?)''',(titulo,conteudo,date))
-
-# NOT USED FOR NOW    
-# AUX Function    
-# get the link for the whole news (not only the resume)
-def getLinksNews(newsFeedX):
-    newsLink = feedparser.parse(newsFeedX)
-    print newsLink['entries'][0]['link'] 
-            
-## This function populate a db
-## without care about updated time
-## just pick up all the news item and put it into the db
-## This function is called once - to poppulate the db in the beginning
-def poppulateDb(newsFeedX, indexGeral=createIndexWhoosh()):
+def poppulateIndex(newsFeedX):
+    """ insert news in the Index without caring about updated time
+        just picking up all the news item from news feed
+        this function is intended to called only once time:
+        - in the beginning of the execution
+        - or - in a real situation - when a new NewsFeed is added to the program
+    """
+    indexGeral=createIndex()
     newsFeed = feedparser.parse(newsFeedX)
     writer = indexGeral.writer()
+    for item in newsFeed.entries:
+        titleFinal = item.title
+        contentIn = item.description
+        contentFinal = remove_html_tags(contentIn)
+        dateIn = item.published_parsed
+        dateFinal = datetime(dateIn[0], dateIn[1], dateIn[2], dateIn[3], dateIn[4], dateIn[5])
+        writer.add_document(content=contentFinal, date=dateFinal, title=titleFinal)
+    writer.commit()
+    
+    
+def collectNewNotices(newsFeedX):
+    """ search if there are new News and collect them 
+        how? check if the news in the feed is already in the Index
+        comparing it dates with the last stored date"""
+    indexGeral=open_dir("index")
+    newsFeed = feedparser.parse(newsFeedX)
+    writer = indexGeral.writer()
+    # this function is defined in NewsSearch module and retrieve the last date insert in index
+    lastDate = NewsSearch.getLastDate()
+    for item in newsFeed.entries:
+        dateIn = item.published_parsed
+        dateFinal = datetime(dateIn[0], dateIn[1], dateIn[2], dateIn[3], dateIn[4], dateIn[5])
+        # only adds a item if it's more recent that the ones stored in the index
+        if(dateFinal > lastDate):
+            titleFinal = item.title
+            contentIn = item.description
+            contentFinal = remove_html_tags(contentIn)
+            writer.add_document(content=contentFinal, date=dateFinal, title=titleFinal)
+    writer.commit()
+    
+    
+# ## AUXILIAR FUNCTIONS
+
+def getLinksNews(newsFeedX):
+    """ NOT USED FOR NOW       
+        get the link for the whole news (not only the resume) """
+    newsLink = feedparser.parse(newsFeedX)
+    print newsLink['entries'][0]['link'] 
+    
+def remove_html_tags(data):
+    """ removes html tags of feeds elements """
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+    
+
+ 
+#def main():
+   # poppulateIndex(newsFeedJN)
+    #NewsSearch.printAll()
+    #collectNewNotices(newsFeedDN)
+    #NewsSearch.printAll()
+
+#main()
+
+
+
+#===============================================================================
+#  SQLITE FUNCTIONS - NOT USED NOW - MAYBE WE WILL HAVE THE OPTION TO CHOOSE
+#===============================================================================
+
+# initializing the database
+conn = sqlite3.connect("teste.db")
+# initializing the cursor
+cursor = conn.cursor()
+
+# create a new Table with titulo, conteudo, data
+# this function is just called once
+def createTable():
+    # create a table
+    # cursor.execute("""CREATE TABLE newsfeeds
+    #                 (newsfeedname text, title text, content text, date text) 
+    #              """)
+    cursor.execute('''CREATE TABLE news (titulo TEXT, conteudo TEXT, date TEXT)''')
+    
+# AUX Function
+# Insert new 'New' on the table news
+def addNew(titulo, conteudo, date):
+    cursor.execute('''INSERT INTO news (titulo, conteudo, date)
+    VALUES (?,?,?)''', (titulo, conteudo, date))
+    
+# # This function populate a db
+# # without care about updated time
+# # just pick up all the news item and put it into the db
+# # This function is called once - to poppulate the db in the beginning
+def poppulateDb(newsFeedX):
+    newsFeed = feedparser.parse(newsFeedX)
     for item in newsFeed.entries:
         titleIn = item.title
         contentIn = item.description
         contentOut = remove_html_tags(contentIn)
-        print contentOut
+        # print contentOut
         dateIn = item.published_parsed
-        dateFinal = datetime(dateIn[0], dateIn[1], dateIn[2], dateIn[3], dateIn[4],dateIn[5])
+        dateFinal = datetime(dateIn[0], dateIn[1], dateIn[2], dateIn[3], dateIn[4], dateIn[5])
         # insert some data in database
-        addNew(titleIn,contentOut,dateFinal)
-        # whoosh
-        writer.add_document(content=contentOut.decode('latin-1'), date=dateFinal, title=titleIn)
-    writer.commit()
-
+        addNew(titleIn, contentOut, dateFinal)
+        
 # SERVE APENAS PARA FAZER TESTES DE METER COISAS NA DB
 def populateTeste():
-    d=feedparser.parse(newsFeedJN)
+    d = feedparser.parse(newsFeedJN)
     titulo = d['entries'][2]['title']
     conteudo = d['entries'][2]['description']
     contentOut = remove_html_tags(conteudo)
     dateIn = d['entries'][2]['published_parsed']
-    dataFinal = datetime(dateIn[0], dateIn[1], dateIn[2], dateIn[3], dateIn[4],dateIn[5])
-    addNew(titulo,contentOut,dataFinal)
-
+    dataFinal = datetime(dateIn[0], dateIn[1], dateIn[2], dateIn[3], dateIn[4], dateIn[5])
+    addNew(titulo, contentOut, dataFinal)
+    
 # Print all db elements    
 def getAllDb():
     cursor.execute('SELECT * FROM news ORDER BY date DESC')
@@ -112,81 +181,27 @@ def getAllDb():
         print "\n"
         for j in i:
             print j
-
+            
 def getLastUpdatedDate():
     cursor.execute('SELECT date FROM news ORDER BY date DESC LIMIT 1')
     for i in cursor:
         # Converte o tuplo para string 
         tuploTOstr = "".join(i)
         # Converte a string unicode para datetime
-        date=datetime.strptime(tuploTOstr, '%Y-%m-%d %H:%M:%S')
+        date = datetime.strptime(tuploTOstr, '%Y-%m-%d %H:%M:%S')
         return date
-
-# Funtion to remove htlm tags of feeds elements
-def remove_html_tags(data):
-    p = re.compile(r'<.*?>')
-    return p.sub('', data)
-
+    
 # function to search if there are new news and collect them
-def collectNewItems(newsFeedX, indexGeral = open_dir("index")):
+def collectNewItemsSqLite(newsFeedX):
     newsFeed = feedparser.parse(newsFeedX)
     lastDbDate = getLastUpdatedDate()
-    writer = indexGeral.writer()
     for item in newsFeed.entries:
         dateIn = item.published_parsed
-        dateFinal = datetime(dateIn[0], dateIn[1], dateIn[2], dateIn[3], dateIn[4],dateIn[5])
+        dateFinal = datetime(dateIn[0], dateIn[1], dateIn[2], dateIn[3], dateIn[4], dateIn[5])
         if(dateFinal > lastDbDate):
             titleIn = item.title
             contentIn = item.description
             contentOut = remove_html_tags(contentIn)
             # insert some data
-            addNew(titleIn,contentOut,dateFinal)
-            # whoosh
-            writer.add_document(content=contentOut, date=dateFinal, title=titleIn)
-    writer.commit()
+            addNew(titleIn, contentOut, dateFinal)
             
-            
-def query_func(user_input):
-    ix = open_dir("index")
-    with ix.searcher() as searcher: 
-        query = QueryParser("content", ix.schema, group=OrGroup).parse(u""+user_input) 
-        results = searcher.search(query) 
-        for r in results:
-            print r
-    
-
- 
-def main():
-    
-    
-    #getLinksNews(newsFeedJN)
-    
-    #createTable()
-    
-    #populateTeste()
-    #conn.commit()
-    
-    #######
-    poppulateDb(newsFeedJN)
-    conn.commit()
-    poppulateDb(newsFeedDN)
-    conn.commit()
-
-    #collectNewItems(newsFeedJN)
-    #conn.commit
-    
-    #a=getLastUpdatedDec()
-    #remove_html_tags(a)
-    
-    #getAllDb()
-    
-    #getLastUpdatedDate()
-        
-    #url = "http://feeds.dn.pt/~r/DN-Politica/~3/JzTprZ3hDqQ/story01.htm"    
-    #html = urlopen(url).read()    
-    #raw = nltk.clean_html(html)  
-    #print(raw)
-    
-    query_func("leu")
-
-main()
