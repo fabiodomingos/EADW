66eef609e66810861f0ea83a97deae939a54f2e7
Fábio Domingos <fabio.domingos@ist.utl.pt>
2013-05-13 19:20:34 +0100

    Esqueleto da Interface final

--- src/Main.py
+++ src/Main.py
@@ -29,93 +29,83 @@ token = NLP_PT.tokenizerPT()
 sentilexfile='SentiLex-PT02/SentiLex-lem-PT02.txt'
 sentilexDic = SentimentAnalysis.extractSentilex(sentilexfile)
 
-# FUN‚ÌO MENU SEARCH QUE CONTEM AS OP‚ÍES DENTRO DE UMA PESQUISA
-def menuSearch(search):
-    NewsSearch.searchByRelevance(search)
-    while(1):
-        print "MENU SEARCH"
-        print "1 - Search Personalities"
-        print "2- Qualify News"
-        print "3 - Most Relevant Personalitie"
-        print "0 - Return to Main Menu"
-        
-        option = raw_input("Select an option: ")
-        
-        if option == "1":
-            NewsSearch.searchByPersonalitie(search)
-            continue
-        
-        elif option == "2":
-            NewsSearch.searchByQualify(search, sentilexDic)
-            continue
-        
-        elif option == "3":
-            print "FAZER FUN‚ÌO PARA A PERSONALIDADE MAIS RELEVANTE"
-            continue
-        
-        elif option == "0":
-            return
-        
-        else:
-            print "Invalid Input: Insert the option Number.\n"
-            continue
-            
-        
-# FUN‚ÌO MENU PRINCIPAL ONDE ESTÌO AS OP‚ÍES DE ADICIONAR NOVAS NOTICIAS OU PESQUISAR NAS NOTICIAS    
-def menuPrincipal():
-    while(1):
-        print "MENU PRINCIPAL"
-        print "1 - Collect new News"
-        print "2 - Search in the News"
-        print "3 - Exit"
-            
-        option = raw_input("Select an option: ")
-    
-        if option == "1":
-            print "Tem ao seu dipor o JN e o DN"
-            feed = raw_input("Select your Newspaper: ")
-            if feed=="DN":
-                print "PENSAR COMO FAZER O COLLECT"
-                continue
-            elif feed=="JN":
-                print "PENSAR COMO FAZER O COLLECT"
-                continue
-            else:
-                print "Invalid Input: You need to choose JN or DN"
-        
-        elif option == "2":
-            search = raw_input("Find news with word: ")
-            menuSearch(search)
-        
-        elif option == "3":
-            return
-        
-        else:
-            print "Invalid Input: Insert the option Number.\n"
-            continue
-            
-def main():
-    # VAI ARRANCAR A INTERFACE COM AS OP‚ÍES DE USAR UMA DB EXISTENTE OU CRIAR UMA NOVA DB 
-    while(1):
-        print "DATA MINING - INTERFACE"
-        print "1 - Use a DB system"
-        print "2 - Create a new DB"
-    
-        option = raw_input("Select an option: ")
-    
-        if option == "1":
-            print "FALTA FAZER IR BUSCAR VARIAS DB E CHAMA MENU PRINCIPAL "
-            menuPrincipal()
-            break
-        
-        elif option == "2":
-            print "FALTA CRIAR NOVA DB E CHAMA MENU PRINCIPAL"
-            menuPrincipal()
-            break
-        
-        else:
-            print "Invalid Input: Insert the option Number.\n"
-            continue
+NewsCollectionAndStorage.collectNewNotices(JN)
+NewsCollectionAndStorage.collectNewNotices(DN)
 
-            
+while(1):
+    print 'INTERFACE - DATA MINING POLITICS SEARCH'
+    print '1 - New Search'
+    print '2 - History'
+    print '0 - Exit'
+
+    option = raw_input("Select an option: ")
+
+    # OPCAO POR UMA NOVA PESQUISA
+    if option == "1":
+        
+        # USER DA UMA PALAVRA PARA PESQUISA
+        search = raw_input("Insert a word for search: ")
+        #NewsSearch.search(search)
+        
+        # MENU SEARCH
+        while (1):
+            print '\n'
+            print "SEARCH MENU"
+            print "1 - View News"
+            print "2 - Search Personalities"
+            print "3 - Qualify News"
+            print "4 - Most Influente Personalitie"
+            print "5 - Most Influente Political Party"
+            print "0 - Return to INTERFACE"
+
+            menuoption = raw_input("Select an option: ")
+
+            # OPCAO DE VER NOTICIAS DA PESQUISA
+            if menuoption == "1":
+                print 'VER NOTICIAS - IMPRIME TITULO, CONTEUDO, DATA'
+                continue
+
+            # OPCAO DE PESQUISAR PESSOAS
+            elif menuoption == "2":
+                print "PESQUISAR PESSOAS - IMPRIME TITULO, PESSOAS"
+                continue
+
+            # OPCAO DE CLASSIFICAR NOTICIA
+            elif menuoption == "3":
+                print "CLASSIFICAR NOTICIA - IMPRIME TITULO, CLASSIFICACAO"
+                continue
+
+            # OPCAO DE PESSOA MAIS INFLUENTE
+            elif menuoption == "4":
+                print "PESSOA MAIS INFLUENTE - IMPRIME PESSOA"
+                continue
+
+            # OPCAO DE PARTIDO MAIS INFLUENTE
+            elif menuoption == "5":
+                print "PARTIDO MAIS INFLUENTE - IMPRIME PARTIDO"
+                continue
+
+            # OPCAO DE SAIR
+            elif menuoption == "0":
+                break
+
+            # OPCAO INVALIDA
+            else:
+                print "Invalid Input: Insert the option Number.\n"
+                continue
+
+    # OPCAO POR VER O HISTORICO
+    elif option == "2":
+        print 'HISTORY'
+        print 'IMPRIME HISTORICO - WORDS, PESSOAS, PARTIDOS, MOST RELEVANT PERSON, MOST RELEVANT PARTY'
+
+    # OPCAO SAIR 
+    elif option == "0":
+        break
+
+    # OPCAO INVALIDA 
+    else:
+        print "Invalid Input: Insert the option Number.\n"
+        continue
+
             
